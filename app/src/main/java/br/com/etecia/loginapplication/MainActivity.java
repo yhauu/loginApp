82ed6867a5c9ff87f74b46266beba49e25b148d7
Fabricio Belomo <fabriciobelomo@gmail.com>
2019-04-02 22:23:02 -0300

    Adicionado a nova pagina de bem-vindo

--- app/src/main/java/br/com/etecia/loginapplication/MainActivity.java
+++ app/src/main/java/br/com/etecia/loginapplication/MainActivity.java
@@ -2,6 +2,9 @@ package br.com.etecia.loginapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonLogin = findViewById(R.id.btnLogin);
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                TextView txtLogin = findViewById(R.id.txtLogin);
+                TextView txtSenha = findViewById(R.id.txtSenha);
+                String login = txtLogin.getText().toString();
+                String senha = txtSenha.getText().toString();
+
+                if (login.equals("etecia") && senha.equals("123")){
+                    //activity_home nPagina = new activity_home();
+                }
+            }
+        });
     }
 }
